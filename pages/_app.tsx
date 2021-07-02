da9aab1ffd1c0a65e9cb11c6d235d14434eba039
Ridho Putra <ridho.putra@xtremax.com>
2021-07-02 13:02:56 +0700

    apply rubik font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../utils/theme";
 
+import "@fontsource/rubik/400.css";
+import "@fontsource/rubik/500.css";
+import "@fontsource/rubik/700.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
